53af48de3a98dc7ce8e80b7ed475793daa78639a
WendelSantos366 <wendel.alexandre@icloud.com>
2025-07-22 11:44:42 +0100

    Comentário nome
    Comentário informando que a mensagem solicita que o usuário informe seu nome

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+#mensagem de saudação a turma do python de 2025
 print("Olá turma do Python!")
 print("Tudo bem!")
+#solicita que o usuário informe seu nome 
 nome: str = input("Qual é o seu nome? ")
 print(f"Olá, {nome}!")
