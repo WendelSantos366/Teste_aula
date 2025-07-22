3f31be7b46ff136fdf5532a583f138c8b7b7dca3
WendelSantos366 <wendel.alexandre@icloud.com>
2025-07-22 11:53:07 +0100

    Solicitar idade
    Adicionei o código para que o usuário informe sua idade.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 #mensagem de saudação a turma do python de 2025
 print("Olá turma do Python!")
 print("Tudo bem!")
+#solicita que o usuário informe seu nome
 nome: str = input("Qual é o seu nome? ")
 print(f"Olá, {nome}!")
+#solicita que o usuário informe qual a sua idade
+idade: int = input("Quantos anos voçê tem? ")
+print(f"Sua idade é {idade}!")
